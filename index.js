7c2dc54ffa2c8d68a96e9a8642ddff1bf84132ae
Eduardo de Britto Castro <eduardobcastro@yahoo.com.br>
2018-07-12 11:05:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const { request } = require('https')
+
+function jsonDownload(options) {
+  return new Promise((resolve, reject) => {   
+    let req = request(options, function (res) {
+      let chunks = []
+    
+      res.on('data', function (chunk) {
+        chunks.push(chunk)
+      })
+    
+      res.on('end', function () {
+        let str = Buffer.concat(chunks).toString()
+        try {
+          let obj = JSON.parse(str)
+          resolve(obj)
+        } catch (err) {
+          reject (err)
+        }
+      })
+    
+      req.on('error', function (err) {
+        reject(err)
+      })
+    })
+    req.end()
+  })
+}
+
+function wikiSearh(term) {
+  let options = {
+    'method': 'GET',
+    'hostname': 'www.wikidata.org',
+    'path': `/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${term}`
+  }
+  return jsonDownload(options)
+}
+
+wikiSearh('Lisboa')
+.then(results => {
+  for (item of results.query.search) {
+    console.log(item.snippet)
+  }
+})
+.catch(console.error)
+
